0f0c5de59deaf4cfff85c942fca1ddb1276457ce
Carlos Pinahco <cpinacho@multikrd.com>
2024-04-09 10:34:47 -0500

    Fix settings connection database

--- main_app/db_tunnel.py
+++ main_app/db_tunnel.py
@@ -37,6 +37,7 @@ def  get_tunnel_db_config(server):
                 "MIGRATE": True, 
                 "MIRROR": None, 
                 "NAME": None
-            }
+            },
+            "AUTOCOMMIT": False,
         }
     return {}
