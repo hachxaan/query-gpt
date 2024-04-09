6bfe47355c645a3010006a7131fba949233a5267
Carlos Pinahco <cpinacho@multikrd.com>
2024-04-08 21:54:02 -0500

    Add ssh access

--- main_app/views.py
+++ main_app/views.py
@@ -42,7 +42,10 @@ def download_results(request, query_id):
             ] for row in results
         ]
 
-        filename = "algffa" # f"{re.sub(r'[^\w\s-]', '', query.title).replace(' ', '_')}_{datetime.datetime.now().strftime('%Y%m%d%H%M')}.csv"
+        # filename = f"{re.sub(r'[^\w\s-]', '', query.title).replace(' ', '_')}_{datetime.datetime.now().strftime('%Y%m%d%H%M')}.csv"
+        cleaned_title = re.sub(r'[^\w\s-]', '', query.title).replace(' ', '_')
+        timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M')
+        filename = cleaned_title + '_' + timestamp + '.csv'
 
 
         response = HttpResponse(content_type="text/csv")
