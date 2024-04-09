6e207e413125af71310c8bdfdc424518ceeab2a7
Carlos Pinahco <cpinacho@multikrd.com>
2024-04-08 21:51:56 -0500

    Add ssh access

--- main_app/views.py
+++ main_app/views.py
@@ -43,7 +43,10 @@ def download_results(request, query_id):
         ]
 
         # filename = f"{re.sub(r'[^\w\s-]', '', query.title).replace(' ', '_')}_{datetime.datetime.now().strftime('%Y%m%d%H%M')}.csv"
-        filename = f"{re.sub(r'[^\w\s-]', '', query.title).replace(' ', '_')}_{datetime.datetime.now().strftime('%%Y%%m%%d%%H%%M')}.csv"
+        cleaned_title = re.sub(r'[^\w\s-]', '', query.title).replace(' ', '_')
+        timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M')
+        filename = cleaned_title + '_' + timestamp + '.csv'
+
 
         response = HttpResponse(content_type="text/csv")
         response["Content-Disposition"] = f'attachment; filename="{filename}"'
