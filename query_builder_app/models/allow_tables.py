bf72da60dd66cb2f48cc2f284e39b7286a4fc86b
Carlos Pinahco <cpinacho@multikrd.com>
2024-07-15 18:48:13 -0500

    feat: Add unique constraint to AllowedTable model

--- query_builder_app/models/allow_tables.py
+++ query_builder_app/models/allow_tables.py
@@ -5,11 +5,10 @@ from django.db import models
 class AllowedTable(models.Model):
     class Meta:
         db_table = 'allowed_table'
+        unique_together = ("table", "name")
 
     name = models.CharField(max_length=128, unique=True)
 
-    class Meta:
-        unique_together = ("table", "name")
 
     def __str__(self):
         return self.name
