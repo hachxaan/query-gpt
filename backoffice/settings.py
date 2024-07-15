b96bb93a1bca97581283cfa5c1d5240da39140da
Carlos Pinahco <cpinacho@multikrd.com>
2024-07-15 18:37:06 -0500

    chore: Update staticfiles directory in settings.py

--- backoffice/settings.py
+++ backoffice/settings.py
@@ -204,7 +204,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 print(STATIC_ROOT)
 # Default primary key field type
