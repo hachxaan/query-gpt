b1e201f2f7e61f3704cab8b83983d759ca79b7cf
Carlos Pinahco <cpinacho@multikrd.com>
2024-07-15 19:13:25 -0500

    feat: Add download_results view for query results download

--- query_builder_app/urls.py
+++ query_builder_app/urls.py
@@ -3,8 +3,8 @@
 from django.urls import path
 
 from query_builder_app.views.mailing_audiencias.views import GenerateAndDownloadCSVAudiencesView
-from query_builder_app.views.query_list import query_list, query_create, query_update, query_delete, get_query_from_gpt
-from query_builder_app.views.query_list_download import query_list_download
+from query_builder_app.views.query_list import  query_list, query_create, query_update, query_delete, get_query_from_gpt
+from query_builder_app.views.query_list_download import query_list_download, download_results
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -16,6 +16,9 @@ urlpatterns = [
     path('queries/<int:query_id>/edit/', query_update, name='query_update'),
     path('queries/<int:query_id>/delete/', query_delete, name='query_delete'),
     path('queries/get_query_from_gpt/', get_query_from_gpt, name='get_query_from_gpt'),
+
+
+    path("api/download_results/<int:query_id>/", download_results, name="download_results"),
 
     path('generate-csv-audiences/', GenerateAndDownloadCSVAudiencesView.as_view(), name='generate_csv_audiences'),
     path('audiences/', TemplateView.as_view(template_name='queries/audiences.html'), name='audiences'),
